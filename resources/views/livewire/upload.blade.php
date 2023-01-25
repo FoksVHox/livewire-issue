121678614283a3ff846d1bbdd038eaca69b59dfb
Jimmi <jimmi2908@gmail.com>
2023-01-25 22:47:07 +0100

    Show livewire not working correctly v2 (it doesn't work now)

--- resources/views/livewire/upload.blade.php
+++ resources/views/livewire/upload.blade.php
@@ -4,20 +4,4 @@
     @error('photo') <span class="error">{{ $message }}</span> @enderror
 
     <button type="submit">Save Photo</button>
-
-    <script>
-        Vapor.store(this.$refs.file.files[0], {
-            progress: progress => {
-                this.uploadProgress = Math.round(progress * 100);
-            }
-        }).then(response => {
-            axios.post('/api/profile-photo', {
-                uuid: response.uuid,
-                key: response.key,
-                bucket: response.bucket,
-                name: this.$refs.file.files[0].name,
-                content_type: this.$refs.file.files[0].type,
-            })
-        });
-    </script>
 </form>
